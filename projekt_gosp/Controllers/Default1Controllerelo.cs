72db0fd511865abae8f135caf43067f79600852e
onetaps <home.ufclan@gmail.com>
2016-04-20 14:57:45 +0200

    testelo

---
+++ projekt_gosp/Controllers/Default1Controllerelo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt_gosp.Controllers
+{
+    public class Default1Controllerelo : Controller
+    {
+        //
+        // GET: /Default1Controllerelo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
