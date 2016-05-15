5b52ccac28f880b7e8b8386377b4f5e6a00bfa8a
Mateusz Torbiarczyk <200363@student.pwr.wroc.pl>
2016-05-15 20:38:37 +0200

    Podlgad produktow ktory konczy sie waznosc za tydzien

---
+++ projekt_gosp/Controllers/ExpireDateController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Http.WebHost;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using projekt_gosp.Models;
+using projekt_gosp.Helpers;
+using WebMatrix.WebData;
+using Newtonsoft.Json;
+
+namespace projekt_gosp.Controllers
+{
+    public class ExpireDateController : Controller
+    {
+        private db context = new db();
+
+        //
+        // GET: /ExpireDate/
+
+        public ActionResult Index()
+        {
+            int shopId = GlobalMethods.GetShopId(WebSecurity.CurrentUserId, context, WebSecurity.IsAuthenticated, Session);
+            var expireDate = DateTime.Now.Date.AddDays(7);
+            var products = (from b in context.Towary where b.Data_waznosci <= expireDate &&
+                                 b.ID_sklepu == shopId select b).ToList();
+   
+            return View(products);
+        }
+
+    }
+}
